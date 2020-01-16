32016f04097ff66e48024589df7ea088111850f5
Sander Blue <sblue@newrelic.com>
2020-01-16 13:56:32 -0800

    chore(internal/version): skip version test on release branches

--- internal/version/version_test.go
+++ internal/version/version_test.go
@@ -3,6 +3,8 @@
 package version
 
 import (
+	"log"
+	"os"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -12,6 +14,10 @@ var GitTag = "undefined"
 
 func TestVersionTag(t *testing.T) {
 	t.Parallel()
+
+	log.Print("\n\n******")
+	log.Printf("Current branch: %s", os.Getenv("CIRCLE_BRANCH"))
+	log.Print("*******\n\n")
 
 	assert.Equal(t, Version, GitTag)
 }
