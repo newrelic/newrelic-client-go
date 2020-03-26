a330d1f4cab38b0525d892a2e79648fda8112561
Jonathan Thurman <jthurman@newrelic.com>
2020-03-26 12:39:10 -0700

    refactor(alerts): fix alerts tests

--- pkg/alerts/alerts_test.go
+++ pkg/alerts/alerts_test.go
@@ -1,56 +1,32 @@
+// +build unit integration
+
 package alerts
 
 import (
 	"net/http"
 	"net/http/httptest"
-	"os"
 	"testing"
 
 	mock "github.com/newrelic/newrelic-client-go/internal/testing"
-	"github.com/newrelic/newrelic-client-go/pkg/config"
 )
 
 // TODO: This is used by incidents_test.go still, need to refactor
-// nolint
-func newTestClient(handler http.Handler) Alerts {
+func newTestClient(t *testing.T, handler http.Handler) Alerts {
 	ts := httptest.NewServer(handler)
+	tc := mock.NewTestConfig(t, ts)
 
-	return New(config.Config{
-		AdminAPIKey:           "abc123",
-		BaseURL:               ts.URL,
-		InfrastructureBaseURL: ts.URL,
-		UserAgent:             "newrelic/newrelic-client-go",
-	})
+	return New(tc)
 }
 
-// nolint
-func newMockResponse(
-	t *testing.T,
-	mockJSONResponse string,
-	statusCode int,
-) Alerts {
+func newMockResponse(t *testing.T, mockJSONResponse string, statusCode int) Alerts {
 	ts := mock.NewMockServer(t, mockJSONResponse, statusCode)
+	tc := mock.NewTestConfig(t, ts)
 
-	return New(config.Config{
-		AdminAPIKey:           "abc123",
-		BaseURL:               ts.URL,
-		InfrastructureBaseURL: ts.URL,
-		UserAgent:             "newrelic/newrelic-client-go",
-	})
+	return New(tc)
 }
 
-// nolint
 func newIntegrationTestClient(t *testing.T) Alerts {
-	personalAPIKey := os.Getenv("NEW_RELIC_API_KEY")
-	adminAPIKey := os.Getenv("NEW_RELIC_ADMIN_API_KEY")
+	tc := mock.NewIntegrationTestConfig(t)
 
-	if personalAPIKey == "" && adminAPIKey == "" {
-		t.Skipf("acceptance testing requires NEW_RELIC_API_KEY and NEW_RELIC_ADMIN_API_KEY")
-	}
-
-	return New(config.Config{
-		AdminAPIKey:    adminAPIKey,
-		PersonalAPIKey: personalAPIKey,
-		LogLevel:       "debug",
-	})
+	return New(tc)
 }
