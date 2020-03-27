7dce2094ee1c6661306fdcc7e9b82b6e9a2061c2
Jonathan Thurman <jthurman@newrelic.com>
2020-03-27 10:00:29 -0700

    chore(build): Run lint fixes post code generation

--- build/generate.mk
+++ build/generate.mk
@@ -6,9 +6,14 @@ GO           ?= go
 
 PACKAGES ?= $(shell $(GO) list ./...)
 
-generate: tools-compile
+# Generate then lint fixes
+generate: generate-run lint-fix
+
+generate-run: tools-compile
 	@echo "=== $(PROJECT_NAME) === [ generate         ]: Running generate..."
 	@for p in $(PACKAGES); do \
 		echo "=== $(PROJECT_NAME) === [ generate         ]:     $$p"; \
 			$(GO) generate -x $$p ; \
 	done
+
+.PHONY: generate generate-run
