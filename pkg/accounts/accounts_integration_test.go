c6f61e20ec4113c44ea97f8eff04def55aa2ab99
Chris Trombley <ctrombley@newrelic.com>
2020-07-08 20:56:28 -0700

    feat(accounts): add an accounts resource

---
+++ pkg/accounts/accounts_integration_test.go
@@ -0,0 +1,32 @@
+// +build integration
+
+package accounts
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	mock "github.com/newrelic/newrelic-client-go/pkg/testhelpers"
+)
+
+func TestIntegrationAccounts(t *testing.T) {
+	t.Parallel()
+
+	client := newIntegrationTestClient(t)
+
+	// Test: List
+	params := ListAccountsParams{
+		Scope: &RegionScopeTypes.GLOBAL,
+	}
+
+	accounts, err := client.ListAccounts(params)
+	require.NoError(t, err)
+	require.Greater(t, len(accounts), 0)
+}
+
+func newIntegrationTestClient(t *testing.T) Accounts {
+	tc := mock.NewIntegrationTestConfig(t)
+
+	return New(tc)
+}
