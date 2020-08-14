ceb31e7b756a37763a25dc0068e2f57a66d5543b
Mike <msummers@newrelic.com>
2020-08-14 12:26:01 -0500

    feat(logs): implement Log API

---
+++ pkg/logs/example_basic_test.go
@@ -0,0 +1,34 @@
+// +build integration
+
+package logs
+
+import (
+	"log"
+	"os"
+
+	"github.com/newrelic/newrelic-client-go/pkg/config"
+)
+
+//func TestExample_basic(t *testing.T) {
+func Example_basic() {
+	// Initialize the client configuration.  An Insights insert key is required
+	// to communicate with the backend API.
+	cfg := config.New()
+	cfg.LicenseKey = os.Getenv("NEW_RELIC_LICENSE_KEY")
+	cfg.LogLevel = "trace"
+	cfg.Compression = config.Compression.None
+
+	// Initialize the client.
+	client := New(cfg)
+
+	logEntry := struct {
+		Message string `json:"message"`
+	}{
+		Message: "INFO: From example basic test.go",
+	}
+
+	// Post a Log entry
+	if err := client.CreateLogEntry(logEntry); err != nil {
+		log.Fatal("error posting Log entry: ", err)
+	}
+}
