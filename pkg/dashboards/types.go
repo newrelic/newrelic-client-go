0f968b2f62e848b3562b4d65e74b23f923080c71
Jonathan Thurman <jthurman@newrelic.com>
2020-12-23 17:08:00 -0800

    chore(dashboards): Update ID to also be a string...

--- pkg/dashboards/types.go
+++ pkg/dashboards/types.go
@@ -232,7 +232,7 @@ type DashboardWidgetInput struct {
 	// Typed configuration for the widget
 	Configuration DashboardWidgetConfigurationInput `json:"configuration,omitempty"`
 	// id.
-	ID int `json:"id,omitempty"`
+	ID string `json:"id,omitempty"`
 	// layout
 	Layout DashboardWidgetLayoutInput `json:"layout,omitempty"`
 	// Related entities. Currently only supports Dashboard entities, but may allow other cases in the future.
@@ -278,10 +278,3 @@ type DashboardWidgetRawConfiguration string
 // values as specified by
 // [IEEE 754](http://en.wikipedia.org/wiki/IEEE_floating_point).
 type Float string
-
-// ID - The `ID` scalar type represents a unique identifier, often used to
-// refetch an object or as key for a cache. The ID type appears in a JSON
-// response as a String; however, it is not intended to be human-readable.
-// When expected as an input type, any string (such as `"4"`) or integer
-// (such as `4`) input value will be accepted as an ID.
-type ID string
