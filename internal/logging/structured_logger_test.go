f2da9499d204ca0e3d4fdca9b452d4cb202e8888
Zach Leslie <zleslie@newrelic.com>
2021-01-06 13:32:45 -0800

    tests(logging): include tests for StructuredLogger

---
+++ internal/logging/structured_logger_test.go
@@ -0,0 +1,49 @@
+// +build unit
+
+package logging
+
+import (
+	"testing"
+
+	log "github.com/sirupsen/logrus"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewStructuredLogger_private(t *testing.T) {
+	t.Parallel()
+
+	logrusLevel := log.GetLevel()
+
+	l := NewStructuredLogger()
+
+	l.SetLogLevel("debug")
+
+	// Make sure that the global logrus level is left unchanged.
+	assert.Equal(t, log.GetLevel(), logrusLevel)
+
+	loggerLevel := l.logger.GetLevel()
+	assert.Equal(t, loggerLevel, log.DebugLevel)
+}
+
+func TestSetLevel(t *testing.T) {
+	t.Parallel()
+	l := NewStructuredLogger()
+
+	defaultLevel, err := log.ParseLevel(defaultLogLevel)
+
+	l.SetLogLevel("")
+	loggerLevel := l.logger.GetLevel()
+
+	assert.NoError(t, err)
+	assert.Equal(t, loggerLevel, defaultLevel)
+
+	l.SetLogLevel("notdebug")
+	loggerLevel = l.logger.GetLevel()
+	assert.Equal(t, loggerLevel, defaultLevel)
+
+}
+
+func TestStructuredLogger_interface(t *testing.T) {
+	var l Logger = NewStructuredLogger()
+	l.Info("testing")
+}
