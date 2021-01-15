ae7e0063026dea1a754a47e3b77179abdb4adf06
Jonathan Thurman <jthurman@newrelic.com>
2021-01-15 11:12:12 -0800

    refactor(dashboards): Use nrqlQueries in place of queries

--- pkg/dashboards/dashboard.go
+++ pkg/dashboards/dashboard.go
@@ -86,13 +86,13 @@ const getDashboardEntityQuery = `query ($guid: EntityGuid!) {
           updatedAt
           widgets {
             configuration {
-              area { queries { accountId nrql } }
-              bar { queries { accountId nrql } }
-              billboard { queries { accountId nrql } thresholds { alertSeverity value } }
-              line { queries { accountId nrql } }
+              area { nrqlQueries { accountId query } }
+              bar { nrqlQueries { accountId query } }
+              billboard { nrqlQueries { accountId query } thresholds { alertSeverity value } }
+              line { nrqlQueries { accountId query } }
               markdown { text }
-              pie { queries { accountId nrql } }
-              table { queries { accountId nrql } }
+              pie { nrqlQueries { accountId query } }
+              table { nrqlQueries { accountId query } }
             }
             layout { column height row width }
             title
