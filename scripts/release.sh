5b7efe81af6c034144527afbcd1fa85d6ae5c995
Sander Blue <sblue@newrelic.com>
2021-09-23 14:58:09 -0500

    fix: use all branches for tag-mode  to get current and next tag with svu

--- scripts/release.sh
+++ scripts/release.sh
@@ -13,8 +13,8 @@ REL_CMD=${GOBIN}/goreleaser
 RELEASE_NOTES_FILE=${SRCDIR}/tmp/relnotes.md
 
 # Compare versions
-VER_CURR=$(${VER_CMD} current --strip-prefix)
-VER_NEXT=$(${VER_CMD} next --strip-prefix)
+VER_CURR=$(${VER_CMD} current --strip-prefix --tag-mode all-branches)
+VER_NEXT=$(${VER_CMD} next --strip-prefix --tag-mode all-branches)
 
 if [ $CURRENT_GIT_BRANCH != ${DEFAULT_BRANCH} ]; then
   echo "Not on ${DEFAULT_BRANCH}, skipping"
