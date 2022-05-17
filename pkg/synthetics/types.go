be2e32b76e203bfe2af0db1a9d2557a1a3be6a77
K Sandeep Kumar <skumarreddy@newrelic.com>
2022-05-17 15:48:31 +0530

    chore: added integration testing for private locations

--- pkg/synthetics/types.go
+++ pkg/synthetics/types.go
@@ -572,6 +572,12 @@ type SyntheticsScriptedMonitorLocationsInput struct {
 	Public []string `json:"public"`
 }
 
+// SyntheticsPrivateLocationPurgeQueueResult - Result of a Synthetics purge private location queue mutation
+type SyntheticsPrivateLocationPurgeQueueResult struct {
+	// An array containing errors resulting from the mutation, if any
+	Errors []SyntheticsPrivateLocationMutationError `json:"errors,omitempty"`
+}
+
 // SyntheticsSecureCredentialMutationResult - The result of a secure credential mutation
 type SyntheticsSecureCredentialMutationResult struct {
 	// The moment when the secure credential was created, represented in milliseconds since the Unix epoch.
