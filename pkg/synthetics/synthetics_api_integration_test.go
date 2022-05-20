5aa1c571f2f4d56ac496caf6fc6e605fb5742537
shivabandari <sbandari@newrelic.com>
2022-05-20 13:17:47 +0530

    feat: added testing scripts to test synthetic monitors

--- pkg/synthetics/synthetics_api_integration_test.go
+++ pkg/synthetics/synthetics_api_integration_test.go
@@ -4,11 +4,12 @@
 package synthetics
 
 import (
+	"github.com/stretchr/testify/require"
 	"testing"
 
-	"github.com/stretchr/testify/require"
-
+	"fmt"
 	mock "github.com/newrelic/newrelic-client-go/pkg/testhelpers"
+	"os"
 )
 
 func TestSyntheticsSecureCredential_Basic(t *testing.T) {
@@ -39,110 +40,414 @@ func TestSyntheticsSecureCredential_Basic(t *testing.T) {
 	require.Nil(t, deleteResp)
 }
 
-func TestSyntheticsMonitors_Basic(t *testing.T) {
+//Test simple browser monitor
+func TestSyntheticsSimpleBrowserMonitor_Basic(t *testing.T) {
+
+	t.Parallel()
+
+	testAccountID, err := mock.GetTestAccountID()
+	if err != nil {
+		t.Skipf("%s", err)
+	}
+
+	a := newIntegrationTestClient(t)
+
+	////Simple Browser monitor
+	//Input for simple browser monitor
+	simpleBrowserMonitorInput := SyntheticsCreateSimpleBrowserMonitorInput{
+		Locations: SyntheticsLocationsInput{
+			Public: []string{
+				"AP_SOUTH_1",
+			},
+		},
+		Name:   "testSimpleBrowserMonitor",
+		Period: SyntheticsMonitorPeriod(SyntheticsMonitorPeriodTypes.EVERY_5_MINUTES),
+		Status: SyntheticsMonitorStatus(SyntheticsMonitorStatusTypes.ENABLED),
+		Tags: []SyntheticsTag{
+			{
+				Key: "Name",
+				Values: []string{
+					"synthetics",
+				},
+			},
+		},
+		Uri: "https://www.one.newrelic.com",
+		Runtime: SyntheticsRuntimeInput{
+			RuntimeType:        "CHROME_BROWSER",
+			RuntimeTypeVersion: SemVer("100"),
+			ScriptLanguage:     "JAVASCRIPT",
+		},
+		AdvancedOptions: SyntheticsSimpleBrowserMonitorAdvancedOptionsInput{
+			EnableScreenshotOnFailureAndScript: true,
+			ResponseValidationText:             "SUCCESS",
+			CustomHeaders: []SyntheticsCustomHeaderInput{
+				{
+					Name:  "Monitor",
+					Value: "synthetics",
+				},
+			},
+			UseTlsValidation: true,
+		},
+	}
+
+	//Test to create simple browser monitor
+	createSimpleBrowserMonitor, err := a.SyntheticsCreateSimpleBrowserMonitor(testAccountID, simpleBrowserMonitorInput)
+
+	require.Equal(t, 0, len(createSimpleBrowserMonitor.Errors))
+	require.NoError(t, err)
+	require.NotNil(t, createSimpleBrowserMonitor)
+
+	//Input for simple browser monitor for updating
+	simpleBrowserMonitorInputUpdated := SyntheticsUpdateSimpleBrowserMonitorInput{
+		AdvancedOptions: SyntheticsSimpleBrowserMonitorAdvancedOptionsInput{
+			CustomHeaders: []SyntheticsCustomHeaderInput{
+				{
+					Name:  "Monitor",
+					Value: "Synthetics",
+				},
+			},
+			EnableScreenshotOnFailureAndScript: true,
+			ResponseValidationText:             "Success",
+			UseTlsValidation:                   true,
+		},
+		Locations: SyntheticsLocationsInput{
+			Public: []string{
+				"AP_SOUTH_1",
+			},
+		},
+		Name:   "testSimpleBrowserMonitorUpdated",
+		Period: SyntheticsMonitorPeriod(SyntheticsMonitorPeriodTypes.EVERY_5_MINUTES),
+		Status: SyntheticsMonitorStatus(SyntheticsMonitorStatusTypes.ENABLED),
+		Tags: []SyntheticsTag{
+			{
+				Key: "Name",
+				Values: []string{
+					"synthetics",
+				},
+			},
+		},
+		Uri: "https://www.one.newrelic.com",
+		Runtime: SyntheticsRuntimeInput{
+			RuntimeType:        "CHROME_BROWSER",
+			RuntimeTypeVersion: SemVer("100"),
+			ScriptLanguage:     "JAVASCRIPT",
+		},
+	}
+
+	//Test to update simple browser monitor
+	updateSimpleBrowserMonitor, err := a.SyntheticsUpdateSimpleBrowserMonitor(createSimpleBrowserMonitor.Monitor.GUID, simpleBrowserMonitorInputUpdated)
+
+	require.Equal(t, 0, len(updateSimpleBrowserMonitor.Errors))
+	require.NoError(t, err)
+	require.NotNil(t, updateSimpleBrowserMonitor)
+
+	//Test to delete a simple browser monitor
+	deleteSimpleBrowserMonitor, err := a.SyntheticsDeleteMonitor(createSimpleBrowserMonitor.Monitor.GUID)
+
+	require.NotNil(t, deleteSimpleBrowserMonitor)
+	require.NoError(t, err)
+}
+
+//TestSyntheticsSimpleMonitor_Basic function to test simple monitor
+func TestSyntheticsSimpleMonitor_Basic(t *testing.T) {
+
 	t.Parallel()
 	testAccountID, err := mock.GetTestAccountID()
 	if err != nil {
 		t.Skipf("%s", err)
 	}
-	location := SyntheticsLocationsInput{
-		Public: "AP_SOUTH_1",
-	}
-	tags := SyntheticsTag{
-		Key:    "Name",
-		Values: "testSimpleBrowserMonitor",
-	}
-	runtime := SyntheticsRuntimeInput{
-		RuntimeType:        "CHROME_BROWSER",
-		RuntimeTypeVersion: 100,
-		ScriptLanguage:     "JAVASCRIPT",
-	}
 
-	advanceOptions := SyntheticsSimpleBrowserMonitorAdvancedOptionsInput{
-		EnableScreenshotOnFailureAndScript: true,
-		ResponseValidationText:             "Success",
-		CustomHeaders: SyntheticsCustomHeaderInput{
-			Name:  "Monitor",
-			Value: "synthetics",
-		},
-		UseTlsValidation: true,
-	}
-
-	//Create synthetic script api Monitor
-	simpleBrowserMonitor := SyntheticsCreateSimpleBrowserMonitorInput{
-		Locations:       location,
-		Name:            "testSimpleMonitor",
-		Period:          "EVERY_5_MINUTES",
-		Status:          "ENABLED",
-		Tags:            tags,
-		Uri:             "https://www.one.newrelic.com",
-		Runtime:         runtime,
-		AdvancedOptions: advanceOptions,
-	}
 	a := newIntegrationTestClient(t)
 
-	//create a simple browser monitor
-	createSimpleBrowserMonitor, err := a.SyntheticsCreateSimpleBrowserMonitor(testAccountID, simpleBrowserMonitor)
-
-	require.NoError(t, err)
-	require.NotNil(t, createSimpleBrowserMonitor)
-
-	simpleBrowserMonitor := SyntheticsCreateSimpleBrowserMonitorInput{
-		Locations:       location,
-		Name:            "testSimpleMonitor-updated",
-		Period:          "EVERY_5_MINUTES",
-		Status:          "ENABLED",
-		Tags:            tags,
-		Uri:             "https://www.one.newrelic.com",
-		Runtime:         runtime,
-		AdvancedOptions: advanceOptions,
-	}
-	//update a simple browser monitor
-	updateSimpleBrowerMonitor, err := a.SyntheticsUpdateSimpleBrowserMonitor(createSimpleBrowserMonitor.Monitor.GUID, simpleBrowserMonitor)
-
-	require.NoError(t, err)
-	require.NotNil(t, updateSimpleBrowerMonitor)
-
-	//delete a simple browser monitor
-	deleteSimpleBrowserMonitor, err := a.SyntheticsDeleteMonitor(createSimpleBrowserMonitor.Monitor.GUID)
-
-	require.NotNil(t, deleteSimpleBrowserMonitor)
-
-	simpleMonitor := SyntheticsCreateSimpleMonitorInput{
-		Locations:       location,
-		Name:            "testSimpleMonitor",
-		Period:          "EVERY_5_MINUTES",
-		Status:          "ENABLED",
-		Tags:            tags,
-		Uri:             "https://www.one.newrelic.com",
-		AdvancedOptions: advanceOptions,
+	////simple monitor
+	//Input for creating a simple monitor
+	simpleMonitorInput := SyntheticsCreateSimpleMonitorInput{
+		AdvancedOptions: SyntheticsSimpleMonitorAdvancedOptionsInput{
+			CustomHeaders: []SyntheticsCustomHeaderInput{
+				{
+					Name:  "Monitor",
+					Value: "Synthetics",
+				},
+			},
+			ResponseValidationText:  "Success",
+			RedirectIsFailure:       true,
+			ShouldBypassHeadRequest: true,
+			UseTlsValidation:        true,
+		},
+		Locations: SyntheticsLocationsInput{
+			Public: []string{
+				"AP_SOUTH_1",
+			},
+		},
+		Name:   "testSimpleMonitor",
+		Period: SyntheticsMonitorPeriod(SyntheticsMonitorPeriodTypes.EVERY_5_MINUTES),
+		Status: SyntheticsMonitorStatus(SyntheticsMonitorStatusTypes.ENABLED),
+		Tags: []SyntheticsTag{
+			{
+				Key: "Name",
+				Values: []string{
+					"Synthetics",
+				},
+			},
+		},
+		Uri: "https://www.one.newrelic.com",
 	}
 
-	//create a simple monitor
-	createSimpleMonitor, err := a.SyntheticsCreateSimpleMonitor(testAccountID, simpleMonitor)
+	//Test to create simple monitor
+	createSimpleMonitor, err := a.SyntheticsCreateSimpleMonitor(testAccountID, simpleMonitorInput)
 
+	require.Equal(t, 0, len(createSimpleMonitor.Errors))
 	require.NoError(t, err)
 	require.NotNil(t, createSimpleMonitor)
 
-	simpleMonitor := SyntheticsCreateSimpleMonitorInput{
-		Locations:       location,
-		Name:            "testSimpleMonitor-updated",
-		Period:          "EVERY_5_MINUTES",
-		Status:          "ENABLED",
-		Tags:            tags,
-		Uri:             "https://www.one.newrelic.com",
-		AdvancedOptions: advanceOptions,
+	//Input to update simple monitor
+	simpleMonitorInputUpdated := SyntheticsUpdateSimpleMonitorInput{
+		AdvancedOptions: SyntheticsSimpleMonitorAdvancedOptionsInput{
+			CustomHeaders: []SyntheticsCustomHeaderInput{
+				{
+					Name:  "Monitors",
+					Value: "Synthetics",
+				},
+			},
+			ResponseValidationText:  "Success",
+			RedirectIsFailure:       true,
+			ShouldBypassHeadRequest: true,
+			UseTlsValidation:        true,
+		},
+		Locations: SyntheticsLocationsInput{
+			Public: []string{
+				"AP_SOUTH_1",
+			},
+		},
+		Name:   "testSimpleMonitorUpdated",
+		Period: SyntheticsMonitorPeriod(SyntheticsMonitorPeriodTypes.EVERY_5_MINUTES),
+		Status: SyntheticsMonitorStatus(SyntheticsMonitorStatusTypes.ENABLED),
+		Tags: []SyntheticsTag{
+			{
+				Key: "Name",
+				Values: []string{
+					"Synthetics",
+				},
+			},
+		},
+		Uri: "https://www.one.newrelic.com",
 	}
-	//update a simple monitor
-	updateSimpleMonitor, err := a.SyntheticsUpdateSimpleMonitor(createSimpleMonitor.Monitor.GUID, simpleBrowserMonitor)
 
+	//Test to update simple monitor
+	updateSimpleMonitor, err := a.SyntheticsUpdateSimpleMonitor(createSimpleMonitor.Monitor.GUID, simpleMonitorInputUpdated)
+
+	require.Equal(t, 0, len(updateSimpleMonitor.Errors))
 	require.NoError(t, err)
 	require.NotNil(t, updateSimpleMonitor)
 
-	//delete a simple monitor
-	deleteSimpleMonitor, err := a.SyntheticsDeleteMonitor(createSimpleMonitor)
+	//Test to delete simple monitor
+	deleteSimpleMonitor, err := a.SyntheticsDeleteMonitor(createSimpleMonitor.Monitor.GUID)
 
-	require.Nil(t, deleteSimpleMonitor)
+	require.NotNil(t, deleteSimpleMonitor)
+	require.NoError(t, err)
+}
+
+//TestSyntheticsScriptApiMonitor_Basic to test the script api monitor
+func TestSyntheticsScriptApiMonitor_Basic(t *testing.T) {
+
+	t.Parallel()
+	testAccountID, err := mock.GetTestAccountID()
+	if err != nil {
+		t.Skipf("%s", err)
+	}
+
+	a := newIntegrationTestClient(t)
+
+	////Scripted API monitor
+	apiScript := fmt.Sprintf(`
+		const myAccountId = '%s';
+		const myAPIKey = '%s';
+		const options = {
+		// Define endpoint URI, https://api.eu.newrelic.com/graphql for EU accounts
+		uri: 'https://api.newrelic.com/graphql',
+		headers: {
+		'API-key': myAPIKey,
+		'Content-Type': 'application/json',
+		},
+		body: JSON.stringify({
+		query: "
+		query getNrqlResults($accountId: Int!, $nrql: Nrql!) {
+		actor {
+		account(id: $accountId) {
+		nrql(query: $nrql) {results}}}}",
+		variables: {accountId: Number(myAccountId),nrql: 'SELECT average(duration) FROM Transaction'}})};
+		
+		// Define expected results using callback function
+		function callback(err, response, body) {
+		// Log JSON results from endpoint to Synthetics console
+		console.log(body);
+		console.log('Script execution completed');
+		}
+		
+		// Make POST request, passing in options and callback
+		$http.post(options, callback);
+		`, os.Getenv("NEW_RELIC_ACCOUNT_ID"), os.Getenv("NEW_RELIC_API_KEY"))
+
+	//input for script api monitor
+	scriptApiMonitorInput := SyntheticsCreateScriptAPIMonitorInput{
+		Locations: SyntheticsScriptedMonitorLocationsInput{
+			Public: []string{
+				"AP_SOUTH_1",
+			},
+		},
+		Name:   "testScriptApiMonitor",
+		Period: SyntheticsMonitorPeriod(SyntheticsMonitorPeriodTypes.EVERY_5_MINUTES),
+		Status: SyntheticsMonitorStatus(SyntheticsMonitorStatusTypes.ENABLED),
+		Script: apiScript,
+		Tags: []SyntheticsTag{
+			{
+				Key: "Name",
+				Values: []string{
+					"ScriptAPIMonitor",
+				},
+			},
+		},
+		Runtime: SyntheticsRuntimeInput{
+			RuntimeTypeVersion: SemVer("16.10"),
+			RuntimeType:        "NODE_API",
+			ScriptLanguage:     "JAVASCRIPT",
+		},
+	}
+
+	//Test to Create scripted api monitor
+	createScriptApiMonitor, err := a.SyntheticsCreateScriptAPIMonitor(testAccountID, scriptApiMonitorInput)
+
+	require.NoError(t, err)
+	require.NotNil(t, createScriptApiMonitor)
+	require.Equal(t, 0, len(createScriptApiMonitor.Errors))
+
+	//input to update script api monitor
+	updatedScriptApiMonitorInput := SyntheticsUpdateScriptAPIMonitorInput{
+		Locations: SyntheticsScriptedMonitorLocationsInput{
+			Public: []string{
+				"AP_SOUTH_1",
+			},
+		},
+		Name:   "testScriptApiMonitorUpdated",
+		Period: SyntheticsMonitorPeriod(SyntheticsMonitorPeriodTypes.EVERY_5_MINUTES),
+		Status: SyntheticsMonitorStatus(SyntheticsMonitorStatusTypes.ENABLED),
+		Script: apiScript,
+		Tags: []SyntheticsTag{
+			{
+				Key: "Name",
+				Values: []string{
+					"ScriptAPIMonitor",
+				},
+			},
+		},
+		Runtime: SyntheticsRuntimeInput{
+			RuntimeTypeVersion: SemVer("16.10"),
+			RuntimeType:        "NODE_API",
+			ScriptLanguage:     "JAVASCRIPT",
+		},
+	}
+
+	//Test to update scripted api monitor
+	updateScriptApiMonitor, err := a.SyntheticsUpdateScriptAPIMonitor(createScriptApiMonitor.Monitor.GUID, updatedScriptApiMonitorInput)
+
+	require.NoError(t, err)
+	require.NotNil(t, updateScriptApiMonitor)
+	require.Equal(t, 0, len(updateScriptApiMonitor.Errors))
+
+	//Test to delete scripted api monitor
+	deleteScriptApiMonitor, err := a.SyntheticsDeleteMonitor(updateScriptApiMonitor.Monitor.GUID)
+
+	require.NoError(t, err)
+	require.NotNil(t, deleteScriptApiMonitor)
+}
+
+//TestSyntheticsScriptBrowserMonitor_Basic function to test script browser monitor
+func TestSyntheticsScriptBrowserMonitor_Basic(t *testing.T) {
+
+	t.Parallel()
+	testAccountID, err := mock.GetTestAccountID()
+	if err != nil {
+		t.Skipf("%s", err)
+	}
+
+	a := newIntegrationTestClient(t)
+
+	//Input to create script browser monitor
+	scriptBrowserMonitorInput := SyntheticsCreateScriptBrowserMonitorInput{
+		AdvancedOptions: SyntheticsScriptBrowserMonitorAdvancedOptionsInput{
+			EnableScreenshotOnFailureAndScript: true,
+		},
+		Locations: SyntheticsScriptedMonitorLocationsInput{
+			Public: []string{
+				"AP_SOUTH_1",
+			},
+		},
+		Name:   "testScriptBrowserScript",
+		Period: SyntheticsMonitorPeriod(SyntheticsMonitorPeriodTypes.EVERY_5_MINUTES),
+		Status: SyntheticsMonitorStatus(SyntheticsMonitorStatusTypes.ENABLED),
+		Runtime: SyntheticsRuntimeInput{
+			RuntimeTypeVersion: "100",
+			RuntimeType:        "CHROME_BROWSER",
+			ScriptLanguage:     "JAVASCRIPT",
+		},
+		Tags: []SyntheticsTag{
+			{
+				Key: "NAME",
+				Values: []string{
+					"scriptBrowserScript",
+				},
+			},
+		},
+		Script: "var assert = require('assert');\n\n$browser.get('https://one.newrelic.com')",
+	}
+
+	//test to create script browser monitor
+	createScriptBrowserMonitor, err := a.SyntheticsCreateScriptBrowserMonitor(testAccountID, scriptBrowserMonitorInput)
+
+	require.NoError(t, err)
+	require.NotNil(t, createScriptBrowserMonitor)
+	require.Equal(t, 0, len(createScriptBrowserMonitor.Errors))
+
+	//Input to update script browser monitor
+	updatedScriptBrowserMonitorInput := SyntheticsUpdateScriptBrowserMonitorInput{
+		AdvancedOptions: SyntheticsScriptBrowserMonitorAdvancedOptionsInput{
+			EnableScreenshotOnFailureAndScript: true,
+		},
+		Locations: SyntheticsScriptedMonitorLocationsInput{
+			Public: []string{
+				"AP_SOUTH_1",
+			},
+		},
+		Name:   "testScriptBrowserScriptUpdated",
+		Period: SyntheticsMonitorPeriod(SyntheticsMonitorPeriodTypes.EVERY_5_MINUTES),
+		Status: SyntheticsMonitorStatus(SyntheticsMonitorStatusTypes.ENABLED),
+		Runtime: SyntheticsRuntimeInput{
+			RuntimeTypeVersion: "100",
+			RuntimeType:        "CHROME_BROWSER",
+			ScriptLanguage:     "JAVASCRIPT",
+		},
+		Tags: []SyntheticsTag{
+			{
+				Key: "NAME",
+				Values: []string{
+					"script_browser_script",
+				},
+			},
+		},
+		Script: "var assert = require('assert');\n\n$browser.get('https://one.newrelic.com')",
+	}
+	//test to update script browser monitor
+	updateScriptBrowserMonitor, err := a.SyntheticsUpdateScriptBrowserMonitor(createScriptBrowserMonitor.Monitor.GUID, updatedScriptBrowserMonitorInput)
+
+	require.NoError(t, err)
+	require.NotNil(t, updateScriptBrowserMonitor)
+	require.Equal(t, 0, len(updateScriptBrowserMonitor.Errors))
+
+	//test to delete script browser monitor
+	deleteScriptBrowserMonitor, err := a.SyntheticsDeleteMonitor(createScriptBrowserMonitor.Monitor.GUID)
+
+	require.NoError(t, err)
+	require.NotNil(t, deleteScriptBrowserMonitor)
 }
 
 func newIntegrationTestClient(t *testing.T) Synthetics {
