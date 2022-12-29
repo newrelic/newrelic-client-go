96d922e4ff7e134e8b2d4bbbbeedc02f03836b0e
Sander Blue <sblue@newrelic.com>
2022-12-29 13:38:27 -0600

    tests(servicelevel): add error scenario test case for service levels

--- pkg/servicelevel/service_level_integration_test.go
+++ pkg/servicelevel/service_level_integration_test.go
@@ -81,6 +81,51 @@ func TestServiceLevel_Basic(t *testing.T) {
 	require.NotNil(t, deleteResp)
 }
 
+func TestServiceLevel_GoodOrBadEventsRequiredError(t *testing.T) {
+	t.Parallel()
+
+	testAccountID, err := mock.GetTestAccountID()
+	if err != nil {
+		t.Skipf("%s", err)
+	}
+
+	client := newIntegrationTestClient(t)
+
+	// GUID of Dummy App
+	guid := common.EntityGUID("MjUyMDUyOHxBUE18QVBQTElDQVRJT058MjE1MDM3Nzk1")
+
+	// This input is missing some required fields to create a service level (i.e. GoodEvents, BadEvents)
+	createInput := ServiceLevelIndicatorCreateInput{
+		Name:        "integration-test-sli",
+		Description: "integration test service level",
+		Events: ServiceLevelEventsCreateInput{
+			AccountID: testAccountID,
+			ValidEvents: &ServiceLevelEventsQueryCreateInput{
+				From: NRQL("Transaction"),
+			},
+		},
+		Objectives: []ServiceLevelObjectiveCreateInput{
+			{
+				Name:        "intgration-test-sli-objective",
+				Description: "testing",
+				Target:      1.1,
+				TimeWindow: ServiceLevelObjectiveTimeWindowCreateInput{
+					Rolling: ServiceLevelObjectiveRollingTimeWindowCreateInput{
+						Count: 1,
+						Unit:  ServiceLevelObjectiveRollingTimeWindowUnit(ServiceLevelObjectiveRollingTimeWindowUnitTypes.DAY),
+					},
+				},
+			},
+		},
+	}
+
+	// Create
+	createResp, err := client.ServiceLevelCreate(guid, createInput)
+	require.Nil(t, createResp)
+	require.Error(t, err)
+	require.Contains(t, err.Error(), "Defining a new SLI requires a good or bad events query")
+}
+
 func newIntegrationTestClient(t *testing.T) Servicelevel {
 	tc := mock.NewIntegrationTestConfig(t)
 
