dceb483601b4826b2c5651240368fddb9c96eebd
Sander Blue <sblue@newrelic.com>
2023-01-04 15:04:37 -0600

    tests(errors): add additional unit tests

--- pkg/errors/errors_test.go
+++ pkg/errors/errors_test.go
@@ -18,6 +18,14 @@ func TestErrorNotFound(t *testing.T) {
 	assert.Equal(t, "resource not found", e.Error())
 }
 
+func TestErrorNotFoundf(t *testing.T) {
+	t.Parallel()
+
+	e := NewNotFoundf("test %s", "format")
+
+	assert.Equal(t, "test format", e.Error())
+}
+
 func TestErrorInvalidInput(t *testing.T) {
 	t.Parallel()
 
@@ -32,6 +40,14 @@ func TestErrorUnexpectedStatusCode(t *testing.T) {
 	e := NewUnexpectedStatusCode(99, "wat")
 
 	assert.Equal(t, "99 response returned: wat", e.Error())
+}
+
+func TestErrorUnexpectedStatusCodef(t *testing.T) {
+	t.Parallel()
+
+	e := NewUnexpectedStatusCodef(99, "really bad %s", "request")
+
+	assert.Equal(t, "99 response returned: really bad request", e.Error())
 }
 
 func TestErrorUnauthorized(t *testing.T) {
@@ -51,6 +67,14 @@ func TestErrorMaxRetriesReached(t *testing.T) {
 	assert.Equal(t, e.Error(), "maximum retries reached: 2")
 }
 
+func TestErrorMaxRetriesReachedf(t *testing.T) {
+	t.Parallel()
+
+	e := NewMaxRetriesReachedf("2 + %d", 2)
+
+	assert.Equal(t, "maximum retries reached: 2 + 2", e.Error())
+}
+
 func TestInvalidInput(t *testing.T) {
 	t.Parallel()
 
@@ -59,10 +83,26 @@ func TestInvalidInput(t *testing.T) {
 	assert.Equal(t, e.Error(), "invalid input error")
 }
 
+func TestInvalidInputf(t *testing.T) {
+	t.Parallel()
+
+	e := NewInvalidInputf("test %s", "format")
+
+	assert.Equal(t, "test format", e.Error())
+}
+
 func TestInvalidInputWithOptionalMsg(t *testing.T) {
 	t.Parallel()
 
 	e := NewInvalidInput("oopsies")
 
 	assert.Equal(t, e.Error(), "oopsies")
+}
+
+func TestPaymentRequiredError(t *testing.T) {
+	t.Parallel()
+
+	e := NewPaymentRequiredError()
+
+	assert.Equal(t, "Payment Required", e.Error())
 }
