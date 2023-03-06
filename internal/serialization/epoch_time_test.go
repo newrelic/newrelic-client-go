c7d0710d72d3f69947a5a5d72172034b180fbb38
Mike Tkachuk <tmymichael@gmail.com>
2023-03-06 17:19:52 +0200

    fix(EpochTime): fix unmarshal/marshal of empty EpochTime (#997)

--- internal/serialization/epoch_time_test.go
+++ internal/serialization/epoch_time_test.go
@@ -4,14 +4,14 @@
 package serialization
 
 import (
-	"strconv"
 	"testing"
 	"time"
 
 	"github.com/stretchr/testify/assert"
 )
 
-var testEpochValues = []struct {
+var testEpochValuesSuccess = []struct {
+	Name   string
 	Bytes  []byte
 	Epoch  EpochTime
 	String string
@@ -20,6 +20,7 @@ var testEpochValues = []struct {
 	Msg    string
 }{
 	{
+		Name:   "success seconds",
 		Bytes:  []byte(`1587654321`), // Seconds
 		Epoch:  EpochTime(time.Unix(1587654321, 0).UTC()),
 		String: "2020-04-23 15:05:21 +0000 UTC",
@@ -28,6 +29,7 @@ var testEpochValues = []struct {
 		Msg:    "Epoch: Seconds",
 	},
 	{
+		Name:   "success milliseconds",
 		Bytes:  []byte(`1587654321012`), // Milliseconds
 		Epoch:  EpochTime(time.Unix(1587654321, 12*int64(time.Millisecond)).UTC()),
 		String: "2020-04-23 15:05:21.012 +0000 UTC",
@@ -36,7 +38,8 @@ var testEpochValues = []struct {
 		Msg:    "Epoch: Millieconds",
 	},
 	{
-		Bytes:  []byte(`1587654321012345678`), // Nanoseconds
+		Name:   "success nanoseconds",
+		Bytes:  []byte(`1587654321000012345`), // Nanoseconds
 		Epoch:  EpochTime(time.Unix(1587654321, 12345).UTC()),
 		String: "2020-04-23 15:05:21.000012345 +0000 UTC",
 		Unix:   1587654321,
@@ -44,11 +47,21 @@ var testEpochValues = []struct {
 		Msg:    "Epoch: Nanoseconds",
 	},
 	{
-		Bytes:  []byte(`asdf`), // Invalid
+		Name:   "success nanoseconds",
+		Bytes:  []byte(`1587654321000012345`), // Nanoseconds
+		Epoch:  EpochTime(time.Unix(1587654321, 12345).UTC()),
+		String: "2020-04-23 15:05:21.000012345 +0000 UTC",
+		Unix:   1587654321,
+		Err:    nil,
+		Msg:    "Epoch: Nanoseconds",
+	},
+	{
+		Name:   "success empty time object",
+		Bytes:  []byte(emptyTimeCase), // Invalid
 		Epoch:  EpochTime{},
 		String: "0001-01-01 00:00:00 +0000 UTC",
 		Unix:   0,
-		Err:    &strconv.NumError{},
+		Err:    nil,
 		Msg:    "Epoch: invalid",
 	},
 }
@@ -56,39 +69,50 @@ var testEpochValues = []struct {
 func TestEpochUnmarshal(t *testing.T) {
 	t.Parallel()
 
-	for _, v := range testEpochValues {
-		var et EpochTime
-		err := et.UnmarshalJSON(v.Bytes)
+	for _, v := range testEpochValuesSuccess {
+		t.Run(v.Name, func(t *testing.T) {
+			var et EpochTime
+			err := et.UnmarshalJSON(v.Bytes)
 
-		if v.Err != nil {
-			assert.Error(t, err)
-		} else {
-			assert.NoError(t, err)
-		}
-		assert.Equal(t, v.Epoch, et, v.Msg)
+			if v.Err != nil {
+				assert.Error(t, err)
+			} else {
+				assert.NoError(t, err)
+			}
+			assert.Equal(t, v.Epoch, et, v.Msg)
+		})
 	}
+
+	t.Run("failed wrong input", func(t *testing.T) {
+		var et EpochTime
+		err := et.UnmarshalJSON([]byte(`asdf`))
+
+		assert.Error(t, err)
+		assert.Equal(t, EpochTime{}, et)
+	})
 }
 
 func TestEpochMarshalJSON(t *testing.T) {
 	t.Parallel()
 
-	for _, v := range testEpochValues {
-		var et EpochTime
-		err := et.UnmarshalJSON(v.Bytes)
+	for _, v := range testEpochValuesSuccess {
+		t.Run(v.Name, func(t *testing.T) {
+			bytes, err := v.Epoch.MarshalJSON()
 
-		if v.Err != nil {
-			assert.Error(t, err)
-		} else {
-			assert.NoError(t, err)
-		}
-		assert.Equal(t, v.Epoch, et, v.Msg)
+			if v.Err != nil {
+				assert.Error(t, err)
+			} else {
+				assert.NoError(t, err)
+			}
+			assert.Equal(t, v.Bytes, bytes, v.Msg)
+		})
 	}
 }
 
 func TestEpochString(t *testing.T) {
 	t.Parallel()
 
-	for _, v := range testEpochValues {
+	for _, v := range testEpochValuesSuccess {
 		if v.Err == nil {
 			res := v.Epoch.String()
 			assert.Equal(t, v.String, res, v.Msg)
@@ -99,7 +123,7 @@ func TestEpochString(t *testing.T) {
 func TestEpochUnix(t *testing.T) {
 	t.Parallel()
 
-	for _, v := range testEpochValues {
+	for _, v := range testEpochValuesSuccess {
 		if v.Err == nil {
 			res := v.Epoch.Unix()
 			assert.Equal(t, v.Unix, res, v.Msg)
--- pkg/alerts/events_test.go
+++ pkg/alerts/events_test.go
@@ -11,8 +11,8 @@ import (
 )
 
 var (
-	testListAlertEventsResponseJSON = `{
-		"alert_events": [
+	testListAlertEventsOneObjectResponseJSON = `{
+		"recent_events": [
 			{
 				"id": 123,
 				"event_type": "event",
@@ -27,40 +27,142 @@ var (
 			}
 		]
 	}`
+	testListAlertEventsMultipleObjectsResponseJSON = `{
+		"recent_events": [
+			{
+				"id": 123,
+				"event_type": "event",
+				"product": "product",
+				"entity_type": "entity",
+				"entity_group_id": 12345,
+				"entity_id": 12345,
+				"priority": "priority",
+				"description": "description",
+				"timestamp": 1575438237690,
+				"incident_id": 12345
+			},{
+				"id":28012408580,
+				"event_type":"INCIDENT_OPEN",
+				"description":"Some Alert Policy (Your Application Name violated Goroutines min count)",
+				"timestamp":1575438237690,
+				"incident_id":2217914781
+      		}
+		]
+	}`
 )
+
+var testListAlertEventsCases = []struct {
+	name       string
+	body       string
+	params     *ListAlertEventsParams
+	expected   []*AlertEvent
+	httpStatus int
+	errStr     string
+}{
+	{
+		name: "success one object",
+		body: testListAlertEventsOneObjectResponseJSON,
+		params: &ListAlertEventsParams{
+			Product:       "test",
+			EntityType:    "test",
+			EntityGroupID: 12345,
+			EntityID:      12345,
+			EventType:     "test",
+			IncidentID:    12345,
+			Page:          1,
+		},
+		expected: []*AlertEvent{
+			{
+				ID:            123,
+				EventType:     "event",
+				Product:       "product",
+				EntityType:    "entity",
+				EntityGroupID: 12345,
+				EntityID:      12345,
+				Priority:      "priority",
+				Description:   "description",
+				Timestamp:     &testTimestamp,
+				IncidentID:    12345,
+			},
+		},
+		httpStatus: http.StatusOK,
+		errStr:     "",
+	},
+	{
+		name: "success multiple objects",
+		body: testListAlertEventsMultipleObjectsResponseJSON,
+		params: &ListAlertEventsParams{
+			Product:       "test",
+			EntityType:    "test",
+			EntityGroupID: 12345,
+			EntityID:      12345,
+			EventType:     "test",
+			IncidentID:    12345,
+			Page:          1,
+		},
+		expected: []*AlertEvent{
+			{
+				ID:            123,
+				EventType:     "event",
+				Product:       "product",
+				EntityType:    "entity",
+				EntityGroupID: 12345,
+				EntityID:      12345,
+				Priority:      "priority",
+				Description:   "description",
+				Timestamp:     &testTimestamp,
+				IncidentID:    12345,
+			},
+			{
+				ID:          28012408580,
+				EventType:   "INCIDENT_OPEN",
+				Description: "Some Alert Policy (Your Application Name violated Goroutines min count)",
+				Timestamp:   &testTimestamp,
+				IncidentID:  2217914781,
+			},
+		},
+		httpStatus: http.StatusOK,
+		errStr:     "",
+	},
+	{
+		name:       "success no records retrieved",
+		body:       "{}",
+		params:     nil,
+		expected:   []*AlertEvent{},
+		httpStatus: http.StatusOK,
+		errStr:     "",
+	},
+	{
+		name:       "failed unmarshal error",
+		body:       "",
+		params:     nil,
+		expected:   nil,
+		httpStatus: http.StatusOK,
+		errStr:     "unexpected end of JSON input",
+	},
+}
 
 func TestListAlertEvents(t *testing.T) {
 	t.Parallel()
-	alerts := newMockResponse(t, testListAlertEventsResponseJSON, http.StatusOK)
+	for _, tc := range testListAlertEventsCases {
+		t.Run(tc.name, func(t *testing.T) {
+			alerts := newMockResponse(t, tc.body, tc.httpStatus)
 
-	expected := []*AlertEvent{
-		{
-			ID:            123,
-			EventType:     "event",
-			Product:       "product",
-			EntityType:    "entity",
-			EntityGroupID: 12345,
-			EntityID:      12345,
-			Priority:      "priority",
-			Description:   "description",
-			Timestamp:     &testTimestamp,
-			IncidentID:    12345,
-		},
+			actual, err := alerts.ListAlertEvents(tc.params)
+
+			if tc.errStr == "" {
+				assert.NoError(t, err)
+				if len(tc.expected) <= 1 {
+					assert.Equal(t, tc.expected, actual)
+				} else {
+					for i := range tc.expected {
+						assert.Equal(t, tc.expected[i], actual[i])
+					}
+				}
+			} else {
+				assert.Equal(t, tc.errStr, err.Error())
+				assert.Empty(t, actual)
+			}
+		})
 	}
-
-	params := &ListAlertEventsParams{
-		Product:       "test",
-		EntityType:    "test",
-		EntityGroupID: 12345,
-		EntityID:      12345,
-		EventType:     "test",
-		IncidentID:    12345,
-		Page:          1,
-	}
-
-	actual, err := alerts.ListAlertEvents(params)
-
-	assert.NoError(t, err)
-	assert.NotNil(t, actual)
-	assert.Equal(t, expected, actual)
 }
