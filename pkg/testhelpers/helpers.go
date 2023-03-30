336d48c778c384696db6aa410c69cf208fb655a0
pranav-new-relic <pnandula@newrelic.com>
2023-03-30 19:03:22 +0530

    fix(apiaccess): corrects incorrect errors thrown by failing API Access Key creation

--- pkg/testhelpers/helpers.go
+++ pkg/testhelpers/helpers.go
@@ -38,6 +38,12 @@ func GetTestAccountID() (int, error) {
 	return getEnvInt("NEW_RELIC_ACCOUNT_ID")
 }
 
+// GetNonExistentIDs returns two non-existent IDs that can be used to test errors raised
+// when Ingest/User keys which do not exist are updated or deleted.
+func GetNonExistentIDs() (string, string) {
+	return "00000FF000F0FFFF000F0F0000FF0FF0000000F000F00F0FF000FFFFF0FF00F0", "00000HH000H0HHHH000H0H0000HH0HH0000000H000H00H0HH000HHHHH0HH00H0"
+}
+
 // getEnvInt helper to DRY up the other env get calls for integers
 func getEnvInt(name string) (int, error) {
 	if name == "" {
