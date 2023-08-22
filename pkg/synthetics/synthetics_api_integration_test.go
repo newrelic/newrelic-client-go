43e336d61cf5a10eda6ab14c9ab67c97ea669611
Alfredo Molero <amolero@newrelic.com>
2023-08-22 16:32:53 -0400

    chore(synthetics): Fix test-integration errors

--- pkg/synthetics/synthetics_api_integration_test.go
+++ pkg/synthetics/synthetics_api_integration_test.go
@@ -100,7 +100,7 @@ func TestSyntheticsSimpleBrowserMonitor_Basic(t *testing.T) {
 			UseTlsValidation: &tv,
 			DeviceEmulation: &SyntheticsDeviceEmulationInput{
 				DeviceOrientation: SyntheticsDeviceOrientationTypes.PORTRAIT,
-				DeviceType:        SyntheticDeviceTypeTypes.MOBILE,
+				DeviceType:        SyntheticsDeviceTypeTypes.MOBILE, SyntheticsDeviceType
 			},
 		},
 	}
