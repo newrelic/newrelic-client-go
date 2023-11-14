8eee3246452d377e4f95615d3670aa8113a92f08
Julien <jerard@newrelic.com>
2023-11-13 16:25:57 -0800

    chore: add documentation

--- pkg/events/events_service.go
+++ pkg/events/events_service.go
@@ -6,11 +6,14 @@ import (
 	"github.com/newrelic/newrelic-client-go/v2/pkg/config"
 )
 
+// EventsAPI provides an interface to enable mocking the service implementation.
+// You should use this interface to invoke methods and substitute with a mock having a compatible implementation for unit testing your usages.
 type EventsAPI interface {
 	CreateEvent(accountID int, event interface{}) error
 	CreateEventWithContext(ctx context.Context, accountID int, event interface{}) error
 }
 
+// Provides the EventsAPI operations for *Events
 func NewEventsService(opts ...config.ConfigOption) (*Events, error) {
 	cfg := config.New()
 
