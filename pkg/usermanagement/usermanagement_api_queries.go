920ba929ba4d8f212359ff16f6fbe50305a57da4
pranav-new-relic <pnandula@newrelic.com>
2024-01-30 23:45:26 +0530

    chore: addition of user management query

--- pkg/usermanagement/usermanagement_api_queries.go
+++ pkg/usermanagement/usermanagement_api_queries.go
@@ -176,3 +176,97 @@ const getGroupsWithUsersQuery = `query(
     }
   }
 }`
+
+// GET User(s)
+
+func (a *Usermanagement) GetUsers(
+	authenticationDomainIDs []string,
+	userIDs []string,
+	nameContains string,
+	emailContains string,
+) (*UserManagementAuthenticationDomains, error) {
+	return a.GetUsersWithContext(context.Background(),
+		authenticationDomainIDs,
+		userIDs,
+		nameContains,
+		emailContains,
+	)
+}
+
+func (a *Usermanagement) GetUsersWithContext(
+	ctx context.Context,
+	authenticationDomainIDs []string,
+	userIDs []string,
+	nameContains string,
+	emailContains string,
+) (*UserManagementAuthenticationDomains, error) {
+
+	resp := authenticationDomainsResponse{}
+	vars := map[string]interface{}{
+		"authenticationDomainIDs": authenticationDomainIDs,
+		"userIDs":                 userIDs,
+		"nameContains":            nameContains,
+		"emailContains":           emailContains,
+	}
+
+	if err := a.client.NerdGraphQueryWithContext(ctx, getUsersQuery, vars, &resp); err != nil {
+		return nil, err
+	}
+
+	return &resp.Actor.Organization.UserManagement.AuthenticationDomains, nil
+}
+
+const getUsersQuery = `query(
+  $authenticationDomainIDs: [ID!],
+  $userIDs: [ID!],
+  $nameContains: String,
+  $emailContains: String
+)
+{
+  actor {
+    organization {
+      userManagement {
+        authenticationDomains(id: $authenticationDomainIDs) {
+          authenticationDomains {
+            users(
+              filter: {
+                email: { contains: $emailContains }
+                name: { contains: $nameContains }
+                id: { in: $userIDs }
+              }
+            ) {
+              users {
+                id
+                emailVerificationState
+                email
+                lastActive
+                name
+                timeZone
+                pendingUpgradeRequest {
+                  id
+                  message
+                  requestedUserType {
+                    displayName
+                    id
+                  }
+                }
+                type {
+                  displayName
+                  id
+                }
+                groups {
+                  groups {
+                    id
+                    displayName
+                  }
+                }
+              }
+            }
+			id
+			name
+          }
+        }
+      }
+    }
+  }
+}`
