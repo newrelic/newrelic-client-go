848f298df94e1ad8d63ad9b4c5493cbf714c5e74
pranav-new-relic <127438038+pranav-new-relic@users.noreply.github.com>
2024-06-06 12:49:59 +0530

    fix(changetracking): alter the functioning of milli and nanoseconds in timestamps (#1151)

--- internal/utils/utils.go
+++ internal/utils/utils.go
@@ -3,6 +3,9 @@ package utils
 import (
 	"strconv"
 	"strings"
+	"time"
+
+	"github.com/newrelic/newrelic-client-go/v2/pkg/nrtime"
 )
 
 // IntArrayToString converts an array of integers
@@ -16,4 +19,22 @@ func IntArrayToString(integers []int) string {
 	}
 
 	return strings.Join(sArray, ",")
+}
+
+// a helper function that is used to populate a timestamp with non-zero milliseconds
+// if the millisecond count has been found zero, usually when generated with time.Time()
+// in Golang which does not have a nanoseconds field; which helps mutations such as those
+// in changetracking, the API of which requires a timestamp with non-zero milliseconds.
+func GetSafeTimestampWithMilliseconds(inputTimestamp nrtime.EpochMilliseconds) nrtime.EpochMilliseconds {
+	timestamp := time.Time(inputTimestamp)
+
+	// since time.Time in Go does not have a milliseconds field, which is why the implementation
+	// of unmarshaling time.Time into a Unix timestamp in the serialization package relies on
+	// nanoseconds to produce a value of milliseconds, we try employing a similar logic below
+
+	if timestamp.Nanosecond() < 100000000 {
+		timestamp = timestamp.Add(time.Nanosecond * 100000000)
+	}
+
+	return nrtime.EpochMilliseconds(timestamp)
 }
--- internal/utils/utils_test.go
+++ internal/utils/utils_test.go
@@ -4,8 +4,11 @@ package utils
 
 import (
 	"testing"
+	"time"
 
+	"github.com/newrelic/newrelic-client-go/v2/pkg/nrtime"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestIntArrayToString(t *testing.T) {
@@ -24,4 +27,78 @@ func TestIntArrayToString(t *testing.T) {
 	// multiple
 	result = IntArrayToString([]int{1, 2, 3, 4})
 	assert.Equal(t, "1,2,3,4", result)
+}
+
+func TestGetSafeTimestampWithMilliseconds_ZeroNanoseconds(t *testing.T) {
+	t.Parallel()
+
+	now := time.Now()
+	actualNanoseconds := 0
+	expectedNanoseconds := actualNanoseconds + 100000000
+
+	actualResult := GetSafeTimestampWithMilliseconds(nrtime.EpochMilliseconds(
+		time.Date(
+			now.Year(),
+			now.Month(),
+			now.Day(),
+			now.Hour()-3,
+			now.Minute()-30,
+			0,
+			actualNanoseconds,
+			time.Local,
+		),
+	))
+
+	expectedResult := nrtime.EpochMilliseconds(
+		time.Date(
+			now.Year(),
+			now.Month(),
+			now.Day(),
+			now.Hour()-3,
+			now.Minute()-30,
+			0,
+			expectedNanoseconds,
+			time.Local,
+		),
+	)
+	require.Equal(t, actualResult, expectedResult)
+}
+
+func TestGetSafeTimestampWithMilliseconds_NonZeroNanoseconds(t *testing.T) {
+	t.Parallel()
+
+	now := time.Now()
+	actualNanoseconds := 123000000
+
+	// equal, since actualNanoSeconds > 100000000
+	expectedNanoseconds := actualNanoseconds
+
+	actualResult := GetSafeTimestampWithMilliseconds(nrtime.EpochMilliseconds(
+		time.Date(
+			now.Year(),
+			now.Month(),
+			now.Day(),
+			now.Hour()-3,
+			now.Minute()-30,
+			0,
+			actualNanoseconds,
+			time.Local,
+		),
+	))
+
+	expectedResult := nrtime.EpochMilliseconds(
+		time.Date(
+			now.Year(),
+			now.Month(),
+			now.Day(),
+			now.Hour()-3,
+			now.Minute()-30,
+			0,
+			expectedNanoseconds,
+			time.Local,
+		),
+	)
+
+	require.Equal(t, actualResult, expectedResult)
+
 }
