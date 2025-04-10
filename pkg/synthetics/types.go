1618e7a9a4d64cfe6e9408351ff02b38e086bf60
rpaliwal <rpaliwal@newrelic.com>
2025-04-10 16:46:57 +0530

    fix(Synthetics): Remove `omitempty` from `ResponseValidationText` to allow unsetting

--- pkg/synthetics/types.go
+++ pkg/synthetics/types.go
@@ -1710,7 +1710,7 @@ type SyntheticsSimpleMonitorAdvancedOptionsInput struct {
 	// Categorize redirects during a monitor job as a failure
 	RedirectIsFailure *bool `json:"redirectIsFailure,omitempty"`
 	// Validation text for monitor to search for at given URI
-	ResponseValidationText string `json:"responseValidationText,omitempty"`
+	ResponseValidationText *string `json:"responseValidationText"`
 	// Monitor should skip default HEAD request and instead use GET verb in check
 	ShouldBypassHeadRequest *bool `json:"shouldBypassHeadRequest,omitempty"`
 	// Monitor should validate SSL certificate chain
