8a520a76280b7346efc52953ac3781b835b12186
NSSPKrishna <pnialdhuri@newrelic.com>
2025-07-07 13:57:20 +0530

    test: Update test

--- pkg/changetracking/changetracking_api_integration_test.go
+++ pkg/changetracking/changetracking_api_integration_test.go
@@ -4,9 +4,6 @@
 package changetracking
 
 import (
-	"encoding/json"
-	"fmt"
-	"log"
 	"regexp"
 	"testing"
 	"time"
@@ -46,43 +43,43 @@ func TestChangeTrackingCreateDeployment_Basic(t *testing.T) {
 	require.Equal(t, res.EntityGUID, input.EntityGUID)
 }
 
-func TestChangeTrackingCreateDeployment_CustomAttributes(t *testing.T) {
-	skipMsg := fmt.Sprintf("Skipping %s until custom attributes are out of limited preview.", t.Name())
-	t.Skip(skipMsg)
-	t.Parallel()
+// func TestChangeTrackingCreateDeployment_CustomAttributes(t *testing.T) {
+// 	skipMsg := fmt.Sprintf("Skipping %s until custom attributes are out of limited preview.", t.Name())
+// 	t.Skip(skipMsg)
+// 	t.Parallel()
 
-	a := newIntegrationTestClient(t)
+// 	a := newIntegrationTestClient(t)
 
-	var customAttributes = `{"a":"1","b":"two","c":"1.5","d":"true"}`
-	attrs := make(map[string]interface{})
-	err := json.Unmarshal([]byte(customAttributes), &attrs)
-	if err != nil {
-		log.Fatal(err)
-	}
+// 	var customAttributes = `{"a":"1","b":"two","c":"1.5","d":"true"}`
+// 	attrs := make(map[string]interface{})
+// 	err := json.Unmarshal([]byte(customAttributes), &attrs)
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
 
-	input := ChangeTrackingDeploymentInput{
-		Changelog:        "test",
-		Commit:           "12345a",
-		CustomAttributes: ChangeTrackingRawCustomAttributesMap(attrs),
-		DeepLink:         "newrelic-client-go",
-		DeploymentType:   ChangeTrackingDeploymentTypeTypes.BASIC,
-		Description:      "This is a test description",
-		EntityGUID:       common.EntityGUID(testhelpers.IntegrationTestApplicationEntityGUIDNew),
-		GroupId:          "deployment",
-		Timestamp:        nrtime.EpochMilliseconds(time.Now()),
-		User:             "newrelic-go-client",
-		Version:          "0.0.1",
-	}
+// 	input := ChangeTrackingDeploymentInput{
+// 		Changelog:        "test",
+// 		Commit:           "12345a",
+// 		CustomAttributes: ChangeTrackingRawCustomAttributesMap(attrs),
+// 		DeepLink:         "newrelic-client-go",
+// 		DeploymentType:   ChangeTrackingDeploymentTypeTypes.BASIC,
+// 		Description:      "This is a test description",
+// 		EntityGUID:       common.EntityGUID(testhelpers.IntegrationTestApplicationEntityGUIDNew),
+// 		GroupId:          "deployment",
+// 		Timestamp:        nrtime.EpochMilliseconds(time.Now()),
+// 		User:             "newrelic-go-client",
+// 		Version:          "0.0.1",
+// 	}
 
-	res, err := a.ChangeTrackingCreateDeployment(
-		ChangeTrackingDataHandlingRules{ValidationFlags: []ChangeTrackingValidationFlag{ChangeTrackingValidationFlagTypes.FAIL_ON_FIELD_LENGTH}},
-		input,
-	)
-	require.NoError(t, err)
+// 	res, err := a.ChangeTrackingCreateDeployment(
+// 		ChangeTrackingDataHandlingRules{ValidationFlags: []ChangeTrackingValidationFlag{ChangeTrackingValidationFlagTypes.FAIL_ON_FIELD_LENGTH}},
+// 		input,
+// 	)
+// 	require.NoError(t, err)
 
-	require.NotNil(t, res)
-	require.Equal(t, res.EntityGUID, input.EntityGUID)
-}
+// 	require.NotNil(t, res)
+// 	require.Equal(t, res.EntityGUID, input.EntityGUID)
+// }
 
 func TestChangeTrackingCreateDeployment_TimestampError(t *testing.T) {
 	t.Parallel()
