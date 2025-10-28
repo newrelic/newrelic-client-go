b989d670ad7f1bc4f4cfbaf78b6c0d9af3c22585
Adarsh Awasthi <aawasthi@newrelic.com>
2025-10-28 12:45:45 +0530

    feat(azure): namefix

--- pkg/cloud/types.go
+++ pkg/cloud/types.go
@@ -6819,7 +6819,7 @@ func UnmarshalCloudIntegrationInterface(b []byte) (*CloudIntegrationInterface, e
 
 			return &xxx, nil
 		case "CloudAzureAutoDiscoveryIntegration":
-			var interfaceType CloudAzureAutodisocoveryIntegration
+			var interfaceType CloudAzureAutoDisocoveryIntegration
 			err = json.Unmarshal(b, &interfaceType)
 			if err != nil {
 				return nil, err
