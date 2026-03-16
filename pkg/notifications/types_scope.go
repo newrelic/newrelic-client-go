06589d650e090e2f153ed4d4a9cdcf58da5d0b61
sdreddy-newrelic <sdurgareddykovvuri@newrelic.com>
2026-03-16 16:40:45 +0530

    Global destinations (#1378)
    feat(notifications): add scope support for global destinations

---
+++ pkg/notifications/types_scope.go
@@ -0,0 +1,24 @@
+package notifications
+
+// EntityScopeTypeInput - Scope type for destinations
+type EntityScopeTypeInput string
+
+var EntityScopeTypeInputTypes = struct {
+	ORGANIZATION EntityScopeTypeInput
+	ACCOUNT      EntityScopeTypeInput
+}{
+	ORGANIZATION: "ORGANIZATION",
+	ACCOUNT:      "ACCOUNT",
+}
+
+// EntityScopeInput - Scope input for destinations
+type EntityScopeInput struct {
+	ID   string               `json:"id"`
+	Type EntityScopeTypeInput `json:"type"`
+}
+
+// EntityScope - Scope response from API
+type EntityScope struct {
+	ID   string               `json:"id,omitempty"`
+	Type EntityScopeTypeInput `json:"type,omitempty"`
+}
