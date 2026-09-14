658a9081a1eb0c19990614350118e6727fd408c6
pranav-new-relic <pnandula@newrelic.com>
2026-09-14 08:43:14 +0530

    fix(scorecards): remove omitempty from Description in TeamEntityUpdateInput
    Empty string must be sent explicitly to clear a team's description.
    With omitempty, clearing description from the Terraform config was silently
    dropped and the old value persisted on the API (drift not correctable).

--- pkg/scorecards/types.go
+++ pkg/scorecards/types.go
@@ -6215,7 +6215,7 @@ type EntityManagementTeamEntityUpdateInput struct {
 	// See aliases in TeamEntity.
 	Aliases []string `json:"aliases,omitempty"`
 	// See description in TeamEntity.
-	Description string `json:"description,omitempty"`
+	Description string `json:"description"`
 	// See externalIntegration in TeamEntity.
 	ExternalIntegration *EntityManagementTeamExternalIntegrationUpdateInput `json:"externalIntegration,omitempty"`
 	// See managers in TeamEntity.
