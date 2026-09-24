361c79d0ee0f348fd6089664e11975b0b895bd6b
asravani-nr <asravani@newrelic.com>
2026-09-24 13:43:55 +0530

    feat(pathpoint): new import schema  (#1443)
    Co-authored-by: pranav-new-relic <pnandula@newrelic.com>

--- newrelic/newrelic.go
+++ newrelic/newrelic.go
@@ -5,6 +5,7 @@ import (
 	"time"
 
 	"github.com/newrelic/newrelic-client-go/v2/pkg/entityrelationship"
+	"github.com/newrelic/newrelic-client-go/v2/pkg/pathpoint"
 	"github.com/newrelic/newrelic-client-go/v2/pkg/users"
 	"github.com/newrelic/newrelic-client-go/v2/pkg/workflowautomation"
 
@@ -93,6 +94,7 @@ type NewRelic struct {
 	EntityRelationship      entityrelationship.Entityrelationship
 	Users                   users.Users
 	WorkflowAutomation      workflowautomation.Workflowautomation
+	PathPoint               pathpoint.Pathpoint
 
 	config config.Config
 }
@@ -150,6 +152,7 @@ func New(opts ...ConfigOption) (*NewRelic, error) {
 		EntityRelationship:      entityrelationship.New(cfg),
 		Users:                   users.New(cfg),
 		WorkflowAutomation:      workflowautomation.New(cfg),
+		PathPoint:               pathpoint.New(cfg),
 	}
 
 	return nr, nil
